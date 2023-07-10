c174387d375fe8632a92202ce7af65fca36cf9c9
sapir mamann <sapir1999@outlook.com>
2023-07-10 20:28:25 +0300

    Handling Input component errors

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -4,21 +4,13 @@ import { ErrorMessage, useField } from "formik";
 
 export const Input = ({ name, label, ...props }) => {
   const [field, meta] = useField(name);
+  const id = `id_${name}`;
 
-  
   return (
     <>
-      <label htmlFor={field.name}>
-        {label}
-      </label>
-      <input
-        {...field}
-        {...props}
-      />
-      <ErrorMessage
-        name={field.name}
-        component="div"
-      />
+      <label htmlFor={id}>{label}</label>
+      <input {...field} {...props} id={id} />
+      <ErrorMessage name={field.name} component="div" />
     </>
   );
 };
